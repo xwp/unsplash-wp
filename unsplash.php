c474c579772593dad3a28f676fac10dd4c96ec10
Pierre Gordon <pierregordon@protonmail.com>
2020-01-20 22:26:08 -0500

    Add integration test for REST API

--- unsplash.php
+++ unsplash.php
@@ -14,8 +14,6 @@
 
 namespace XWP\Unsplash;
 
-use XWP\Unsplash\RestAPI\RestController;
-
 // Support for site-level autoloading.
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
