e65ad42e9958a704961d24f84915413480d7b77e
Pierre Gordon <pierregordon@protonmail.com>
2020-01-22 14:26:14 -0500

    Decouple Docker from integration test suite

--- tests/phpunit/wp-tests-config.php
+++ tests/phpunit/wp-tests-config.php
@@ -5,7 +5,7 @@
  * @package Unsplash
  */
 
-define( 'ABSPATH', '/var/www/html/' );
+define( 'ABSPATH', __DIR__ . '/../../data/wordpress/html/' );
 
 // Test with multisite enabled.
 // Alternatively, use the tests/phpunit/multisite.xml configuration file.
