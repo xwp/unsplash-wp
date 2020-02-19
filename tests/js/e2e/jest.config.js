2733bb1dff5457e1c366b3a780e557414192ab3b
spacedmonkey <jon@spacedmonkey.co.uk>
2020-02-19 19:05:54 +0000

    Run tests in CI mode.

--- tests/js/e2e/jest.config.js
+++ tests/js/e2e/jest.config.js
@@ -1,16 +1,19 @@
 module.exports = {
-	...require( '@wordpress/scripts/config/jest-e2e.config' ),
-	transformIgnorePatterns: [
-		'node_modules',
-	],
+	...require('@wordpress/scripts/config/jest-e2e.config'),
+	transform: {
+		'^.+\\.[jt]sx?$':
+			'<rootDir>/../../../node_modules/@wordpress/scripts/config/babel-transform',
+	},
+	transformIgnorePatterns: ['node_modules'],
 	setupFilesAfterEnv: [
 		'<rootDir>/config/bootstrap.js',
 		'@wordpress/jest-puppeteer-axe',
 		'expect-puppeteer',
 	],
 	testPathIgnorePatterns: [
-		'.git',
-		'node_modules',
+		'<rootDir>/.git',
+		'<rootDir>/node_modules',
+		'<rootDir>/dist',
 	],
-	reporters: [ [ 'jest-silent-reporter', { useDots: true } ] ],
+	reporters: [['jest-silent-reporter', { useDots: true }]],
 };
