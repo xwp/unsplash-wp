905be613cf8cca85d583fdfd71fe433c00c1c703
spacedmonkey <jon@spacedmonkey.co.uk>
2020-02-19 19:54:20 +0000

    Await selectors again.

--- tests/js/e2e/specs/gutenberg/image-block.test.js
+++ tests/js/e2e/specs/gutenberg/image-block.test.js
@@ -4,6 +4,8 @@
 import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
 import { clickButton } from '../../utils';
 
+const MEDIA_LIBRARY_BUTTON = '.editor-media-placeholder__media-library-button';
+
 describe( 'Image Block', () => {
 
 	beforeEach( async () => {
@@ -12,7 +14,11 @@ describe( 'Image Block', () => {
 
 	it( 'should the tab exist', async () => {
 		await insertBlock( 'Image' );
+		// Click the media library button.
+		await page.waitForSelector( MEDIA_LIBRARY_BUTTON );
+		await page.click( MEDIA_LIBRARY_BUTTON );
 		await clickButton('Library');
+		await page.waitForSelector( '.media-modal' );
 		await page.waitForSelector( '#menu-item-unsplash' );
 		// Wait unsplash tab.
 		await expect( page ).toMatchElement( '#menu-item-unsplash' );
