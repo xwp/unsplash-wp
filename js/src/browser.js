f3d802118869b104b1d771c7a67f2adda0802457
Pierre Gordon <pierregordon@protonmail.com>
2020-02-26 04:08:50 -0500

    Default to 'Upload files' tab

--- js/src/browser.js
+++ js/src/browser.js
@@ -7,3 +7,9 @@ import { withUnsplashTab } from './media/helpers';
 wp.media.view.MediaFrame.Post = withUnsplashTab( wp.media.view.MediaFrame.Post );
 // Override media frame in other places the media browser is used (such as in Gutenberg and the Customizer).
 wp.media.view.MediaFrame.Select = withUnsplashTab( wp.media.view.MediaFrame.Select );
+
+/**
+ * Work around that defaults the media browser to the 'Upload files' tab. This resolves cases where the Unsplash tab is
+ * not present in the media browser (such as when choosing audio or video files).
+ */
+wp.media.controller.Library.prototype.defaults.contentUserSetting = false;
