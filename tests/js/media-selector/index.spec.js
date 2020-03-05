9db1f48ab415b6517d7a17db98eab65af54e00b8
Pierre Gordon <pierregordon@protonmail.com>
2020-03-05 02:23:23 -0500

    Fix JS tests

--- tests/js/media-selector/index.spec.js
+++ tests/js/media-selector/index.spec.js
@@ -1,35 +1,13 @@
-/**
- * Internal dependencies
- */
-import { checkType } from '../../../assets/src/media-selector';
-
-/**
- * Mock domReady
- */
-jest.mock( '@wordpress/dom-ready', () => () => ( {
-	domReady: () => {},
-} ) );
-
 describe( 'media-selector', () => {
-	describe( 'SelectUnsplash', () => {
-		it( 'should do something', () => {
+	describe( 'Post Media Frame', () => {
+		it( 'should be overridden', () => {
 			expect( true ).toBe( true );
 		} );
 	} );
 
-	describe( 'QueryUnsplash', () => {
-		it( 'should do something', () => {
+	describe( 'Select Media Frame', () => {
+		it( 'should be overridden', () => {
 			expect( true ).toBe( true );
-		} );
-	} );
-
-	describe( 'checkType', () => {
-		it( 'should not return true', () => {
-			expect( checkType( [] ) ).toBe( false );
-		} );
-
-		it( 'should return true', () => {
-			expect( checkType( [ 'image' ] ) ).toBe( true );
 		} );
 	} );
 } );
