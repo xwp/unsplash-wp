bcbdce14bb784613fe98d9b6fee444ab83251a30
Pierre Gordon <pierregordon@protonmail.com>
2020-03-25 14:15:23 -0400

    Fix lint errors

--- tests/phpunit/php/class-test-rest-controller.php
+++ tests/phpunit/php/class-test-rest-controller.php
@@ -735,13 +735,13 @@ class Test_Rest_Controller extends WP_Test_REST_Controller_Testcase {
 	 * @covers \Unsplash\Rest_Controller::set_unique_media_id()
 	 */
 	public function test_set_unique_media_id() {
-		$photo = $this->get_photo_response();
-		$index = 3;
-		$page = 3;
+		$photo    = $this->get_photo_response();
+		$index    = 3;
+		$page     = 3;
 		$per_page = 10;
 
 		$rest_controller = new Rest_Controller( new Plugin() );
-		$actual = $rest_controller->set_unique_media_id( $photo, $index, $page, $per_page );
+		$actual          = $rest_controller->set_unique_media_id( $photo, $index, $page, $per_page );
 		$this->assertEquals( 'unsplash-23', $actual['id'] );
 		$this->assertEquals( 'rO8TdlRrOo0', $actual['unsplash_id'] );
 	}
