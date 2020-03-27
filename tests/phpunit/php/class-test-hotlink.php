d079af0371ea126506932bc83e20e446373530a4
Jonny Harris <jon@spacedmonkey.co.uk>
2020-03-27 16:27:32 +0000

    Change code coverage

--- tests/phpunit/php/class-test-hotlink.php
+++ tests/phpunit/php/class-test-hotlink.php
@@ -109,6 +109,7 @@ class Test_Hotlink extends \WP_UnitTestCase {
 	 *
 	 * @covers ::hotlink_images_in_content()
 	 * @covers ::replace_image()
+	 * @covers ::get_image_size()
 	 */
 	public function test_the_content() {
 		$second_id  = $this->factory->attachment->create_object(
