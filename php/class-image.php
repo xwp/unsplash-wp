01e06e3733e926e5fac9a83cb936661657ee8770
Jonny Harris <jon@spacedmonkey.co.uk>
2020-04-21 09:03:41 +0100

    Add comment.

--- php/class-image.php
+++ php/class-image.php
@@ -178,6 +178,7 @@ class Image {
 			],
 			'https://unsplash.com/'
 		);
+		// Whitespace at end of caption is required. 
 		/* translators: 1: User URL, 2: User's name, 3: Unsplash URL */
 		return sprintf( __( 'Photo by <a href="%1$s">%2$s</a> on <a href="%3$s">Unsplash</a> ', 'unsplash' ), esc_url( $user_url ), $user_name, esc_url( $url ) );
 	}
