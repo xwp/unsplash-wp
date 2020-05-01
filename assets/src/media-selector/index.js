d94f4b72a30070ed89536f96f77c751937f648f8
Pierre Gordon <pierregordon@protonmail.com>
2020-04-30 22:02:28 -0400

    Override featured image controller

--- assets/src/media-selector/index.js
+++ assets/src/media-selector/index.js
@@ -3,6 +3,7 @@
  */
 import './style.css';
 import withUnsplashTab from './helpers/withUnsplashTab';
+import unsetUnsplashLibrary from './helpers/unsetUnsplashLibrary';
 
 // Override media frames in the respective editors to add the Unsplash tab.
 
@@ -37,38 +38,14 @@ if ( wp.media && wp.media.view && wp.media.view.MediaFrame ) {
 }
 
 // Ensure we don't mess the user's default media library.
-if ( wp.media && wp.media.controller && wp.media.controller.Library ) {
-	wp.media.controller.Library = wp.media.controller.Library.extend( {
-		saveContentMode() {
-			if ( 'browse' !== this.get( 'router' ) ) {
-				return;
-			}
+if ( wp.media && wp.media.controller ) {
+	if ( wp.media.controller.Library ) {
+		wp.media.controller.Library = unsetUnsplashLibrary( wp.media.controller.Library );
+	}
 
-			const mode = this.frame.content.mode(),
-				view = this.frame.router.get();
-
-			// Prevent persisting Unsplash as the default media tab for the user.
-			if ( 'unsplash' !== mode && view && view.get( mode ) ) {
-				window.setUserSetting( 'libraryContent', mode );
-			}
-		},
-
-		activate() {
-			this.syncSelection();
-
-			wp.Uploader.queue.on( 'add', this.uploading, this );
-
-			this.get( 'selection' ).on(
-				'add remove reset',
-				this.refreshContent,
-				this
-			);
-
-			if ( this.get( 'router' ) && this.get( 'contentUserSetting' ) ) {
-				this.frame.on( 'content:activate', this.saveContentMode, this );
-				// Always default to the Unsplash tab.
-				this.set( 'content', 'unsplash' );
-			}
-		},
-	} );
+	if ( wp.media.controller.FeaturedImage ) {
+		wp.media.controller.FeaturedImage = unsetUnsplashLibrary(
+			wp.media.controller.FeaturedImage
+		);
+	}
 }
