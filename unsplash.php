70a6f4cc839992058a20403761029c1c6ad427b9
Derek Herman <derek.herman@xwp.co>
2020-05-05 23:05:16 -0700

    Remove extra space

--- unsplash.php
+++ unsplash.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/xwp/unsplash-wp
  * Description: Unsplash for WordPress.
  * Version: 1.0.0-alpha
- * Author:  XWP
+ * Author: XWP
  * Author URI: https://xwp.co/
  * License: GPLv2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
