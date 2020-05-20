b0637440e0ab7a1421952c3f3dd055944dcd38bf
Leo Postovoit <lpostovoit@gmail.com>
2020-05-20 01:33:19 -0700

    do not bump to beta just yet

--- unsplash.php
+++ unsplash.php
@@ -3,7 +3,7 @@
  * Plugin Name: Unsplash
  * Plugin URI: https://github.com/xwp/unsplash-wp
  * Description: Unsplash for WordPress.
- * Version: 1.0.0-beta
+ * Version: 1.0.0-alpha
  * Author: XWP
  * Author URI: https://xwp.co/
  * License: GPLv2+
